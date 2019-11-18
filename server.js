c87e339a0c10104d5f8f37352eb91a11de548f9b
patpetpalopet <bodin.d@psru.ac.th>
2019-11-18 16:01:32 +0700

    crate

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require("express");
-var app = express();
 var request = require("request");
 var bodyParser = require("body-parser");
 var moment = require("moment");
 
+var app = express();
 // set bodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
